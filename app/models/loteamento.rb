f176e50a1bc64fadc4e5b9aef18123bd4e8f6881
Thiago <tlcrezende@gmail.com>
2023-08-19 20:39:18 -0300

    added valor, qnt lotes and qnt contrato

--- app/models/loteamento.rb
+++ app/models/loteamento.rb
@@ -1,3 +1,4 @@
 class Loteamento < ApplicationRecord
   has_many :lotes, dependent: :destroy
+  has_many :contratos, through: :lotes
 end
--- app/serializers/loteamento_serializer.rb
+++ app/serializers/loteamento_serializer.rb
@@ -1,7 +1,21 @@
 class LoteamentoSerializer < ActiveModel::Serializer
-  attributes :id, :nome, :registro, :tamanho, :lotes
-
+  attributes :id, :nome, :registro, :tamanho, :lotes, :qnt_lotes, :qtd_lotes_com_contrato, :valor
   def lotes
     object.lotes if instance_options[:show_lotes]
+    'depois pesquiso como remover' if !instance_options[:show_lotes]
   end
+
+  #TODO: otimizar query depois para não disparar n+1
+  def qnt_lotes 
+    object.lotes.count 
+  end
+
+  def qtd_lotes_com_contrato
+    object.lotes.count
+  end
+
+  def valor 
+    object.lotes.sum(:valor)
+  end
+
 end
