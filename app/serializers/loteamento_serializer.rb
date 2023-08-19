b56c5005b8e2577c9f631a6db18c21a05ef3c746
Thiago <tlcrezende@gmail.com>
2023-08-19 20:23:00 -0300

    add conditional lotes on loteamento

--- app/serializers/loteamento_serializer.rb
+++ app/serializers/loteamento_serializer.rb
@@ -1,3 +1,7 @@
 class LoteamentoSerializer < ActiveModel::Serializer
   attributes :id, :nome, :registro, :tamanho, :lotes
+
+  def lotes
+    object.lotes if instance_options[:show_lotes]
+  end
 end
