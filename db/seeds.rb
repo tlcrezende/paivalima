e0fa1569ae0dbaeb3727c17029e59fb3ba2686d3
Eduardo <eduardorodriguesanjos@gmail.com>
2023-08-19 14:38:21 -0300

    [feat] seeds cliente

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,77 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Cliente.create(
+  [
+    {
+      nome: 'João da Silva',
+      cpf_cnpj: '12345678901',
+      data_nascimento: '1997-05-28',
+      celular: '21936181803',
+      endereco: 'Rua das Flores, 123'
+    },
+    {
+      nome: 'Maria da Silva',
+      cpf_cnpj: '10987654321',
+      data_nascimento: '1997-05-28',
+      celular: '21936181803',
+      endereco: 'Rua das Flores, 123'
+    },
+    {
+      nome: 'José da Silva 2',
+      cpf_cnpj: '12345678901',
+      data_nascimento: '1997-05-28',
+      celular: '21936181803',
+      endereco: 'Rua das Flores, 123'
+    },
+    {
+      nome: 'Mariazinha',
+      cpf_cnpj: '10987654321',
+      data_nascimento: '1997-05-28',
+      celular: '21936181803',
+      endereco: 'Rua das Flores, 123'
+    },
+    {
+      nome: 'Tia Joana',
+      cpf_cnpj: '12345678901',
+      data_nascimento: '1997-05-28',
+      celular: '21936181803',
+      endereco: 'Rua das Flores, 123'
+    },
+    {
+      nome: 'Zé do queijo',
+      cpf_cnpj: '10987654321',
+      data_nascimento: '1997-05-28',
+      celular: '21936181803',
+      endereco: 'Rua das Flores, 123'
+    },
+    {
+      nome: 'Abizu luizin',
+      cpf_cnpj: '12345678901',
+      data_nascimento: '1997-05-28',
+      celular: '21936181803',
+      endereco: 'Rua das Flores, 123'
+    },
+    {
+      nome: 'Joãozin do crime',
+      cpf_cnpj: '10987654321',
+      data_nascimento: '1997-05-28',
+      celular: '21936181803',
+      endereco: 'Rua das Flores, 123'
+    },
+    {
+      nome: 'Mano véi da várzea',
+      cpf_cnpj: '12345678901',
+      data_nascimento: '1997-05-28',
+      celular: '21936181803',
+      endereco: 'Rua das Flores, 123'
+    },
+    {
+      nome: 'Abestado do sertão',
+      cpf_cnpj: '10987654321',
+      data_nascimento: '1997-05-28',
+      celular: '21936181803',
+      endereco: 'Rua das Flores, 123'
+    }
+  ]
+)
