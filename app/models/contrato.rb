a69db9b99e2cf668859d1aa0f65d0bbf93cb16e4
Eduardo <eduardorodriguesanjos@gmail.com>
2023-08-19 21:26:44 -0300

    [fail] serializer n+1

--- app/models/contrato.rb
+++ app/models/contrato.rb
@@ -1,4 +1,6 @@
 class Contrato < ApplicationRecord
   belongs_to :lote
   belongs_to :cliente
+
+  has_many :pagamentos
 end
--- app/serializers/loteamento_serializer.rb
+++ app/serializers/loteamento_serializer.rb
@@ -1,21 +1,25 @@
 class LoteamentoSerializer < ActiveModel::Serializer
-  attributes :id, :nome, :registro, :tamanho, :lotes, :qtde_lotes, :qtde_lotes_com_contrato, :valor
+  attributes :id, :nome, :registro, :tamanho, :lotes, :qtde_lotes, :qtde_lotes_com_contrato, :valor, :valor_arrecadado
   def lotes
     object.lotes if instance_options[:show_lotes]
-    'depois pesquiso como remover' if !instance_options[:show_lotes]
+    'depois pesquiso como remover' unless instance_options[:show_lotes]
   end
 
-  #TODO: otimizar query depois para não disparar n+1
-  def qtde_lotes 
-    object.lotes.count 
+  # TODO: otimizar query depois para não disparar n+1
+  def qtde_lotes
+    object.lotes.count
   end
 
   def qtde_lotes_com_contrato
     object.contratos.count
   end
 
-  def valor 
+  def valor
     object.lotes.sum(:valor)
   end
 
+  def valor_arrecadado
+    Contrato.includes(:lote, :pagamentos)
+    object.lotes.map(&:contratos).flatten.map(&:pagamentos).flatten.sum(&:valor)
+  end
 end
